d6bde4121211eae5b8b83f936638f1fda1cd08ef
Uladz <lesavik@tutanota.com>
2020-12-05 01:04:13 +0300

    Change dmenu colors

--- config.h
+++ config.h
@@ -1,5 +1,3 @@
-/* See LICENSE file for copyright and license details. */
-
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -7,10 +5,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "serif:size=12" };
 static const char dmenufont[]       = "sans:size=22";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
 
 /* My colors */
 static const char col_white[]       = "#dadada";
@@ -41,6 +35,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "mpv",      NULL,       NULL,       0,            1,           -1 },
 	{ "Utility",  NULL,       NULL,       0,            1,           -1 },
 	{ "Yad",      NULL,       NULL,       0,            1,           -1 },
 	{ "Zathura",  NULL,       NULL,       2,            0,           -1 },
@@ -67,16 +62,16 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/usr/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray, "-nf", col_white, "-sb", col_orange, "-sf", col_gray, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const char *browsercmd[]           = { "firefox-developer-edition", NULL };
 static const char *pingoutcmd[]           = { "st", "-c", "Utility", "-e", "ping", "-c", "16", "-v", "-w", "60", "to-ping", NULL };
-static const char *ncmpcppcmd[]           = { "st", "-e", "ncmpcpp", NULL };
+static const char *ncmpcppcmd[]           = { "st", "-c", "Utility", "-e", "ncmpcpp", NULL };
 
 static const char *mute[]                 = { "amixer", "-q", "set", "Master", "toggle", NULL };
 static const char *volumedown[]           = { "amixer", "-q", "set", "Master", "2%-", "unmute", NULL };
@@ -94,7 +89,7 @@ static const char *zopenmd[]              = { "zopen.sh", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_F5,     spawn,          {.v = zopenmd } },
